5af1c57703ebcd68b0c9a1fa22b8cef0ca1f8239
William Sorto <william.sorto.alv@gmail.com>
2018-08-28 17:40:28 -0600

    se añadio navbar y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { PrivadoPageComponent } from './components/privado-page/privado-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent},
+  { path: 'privado', component: PrivadoPageComponent },
+  { path: '**', component: NotFoundPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
